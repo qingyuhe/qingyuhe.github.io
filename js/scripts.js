dcddfe2500eb376905728c0f2c92a1e7c33a4907
qingyuhe <qingyuhe>
2024-07-03 04:05:36 -0400

    Contact form

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,42 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+const form = document.querySelector("form");
+const fullName = document.getElementById("name"); 
+const email = document.getElementById("email");
+const phone = document.getElementById("phone"); 
+const subject = document.getElementById("subject"); 
+const message = document.getElementById("message"); 
+const success = document.getElementById("submitSuccessMessage")
+
+
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br> 
+        Email: ${email.value}<br>
+        Phone: ${phone.value}<br>
+        Subject: ${subject.value}<br>
+        Message: ${message.value}`;
+
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "hecynthia@gmail.com",
+        Password : "D544330883F888E309F3A0CAB8972B938D3A",
+        To : 'hecynthia@gmail.com',
+        From : "hecynthia@gmail.com",
+        Subject : subject.value,
+        Body : bodyMessage
+    });
+}
+
+// function checkInputs() {
+//     const items = document.querySelectorAll(".form-control");
+
+//     for (const item of items) {
+//         if (item.value == "") {
+//             item.classList.add("error");
+//             item.parentElement.classList.add("error");
+//         }
+//     }
+// }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+    sendEmail(); 
+}); 
